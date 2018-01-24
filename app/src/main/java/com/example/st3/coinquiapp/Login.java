81d798eee325c6eaeb2ea4d7dc41e03ae2b20dcf
tatalessap <martapibirimp@gmail.com>
2018-01-24 16:45:25 +0100

    Prova_marta

---
+++ app/src/main/java/com/example/st3/coinquiapp/Login.java
@@ -0,0 +1,26 @@
+package com.example.st3.coinquiapp;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+/**
+ * Created by marta_nga0hmy on 24/01/2018.
+ */
+
+public class Login extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+}
