846a37fb5aa86d2cb43413d69b78552b1c63049b
StefanoRUsai <stefano.r.usai@gmail.com>
2018-01-27 00:48:38 +0100

    Forse ci sono, activity turni e qualche modifica ai menù

--- app/src/main/java/com/example/st3/coinquiapp/activity/MainActivity.java
+++ app/src/main/java/com/example/st3/coinquiapp/activity/MainActivity.java
@@ -1,5 +1,8 @@
-package com.example.st3.coinquiapp;
+package com.example.st3.coinquiapp.activity;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
@@ -13,6 +16,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.example.st3.coinquiapp.R;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -80,18 +85,12 @@ public class MainActivity extends AppCompatActivity
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
+        FragmentManager fragmentManager= getFragmentManager();
+        if (id == R.id.nav_casa) {
 
-        if (id == R.id.nav_camera) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_personale) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
 
         }
 
@@ -101,7 +100,9 @@ public class MainActivity extends AppCompatActivity
     }
 
     public void onClick(View view){
-        startActivity(new Intent("android.intent.action.TurniActivity"));
-
+        Intent intent = new  Intent(getBaseContext(), Turni.class);
+        startActivity(intent);
     }
+
+
 }
