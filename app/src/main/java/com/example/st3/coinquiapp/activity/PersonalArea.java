9970899ce540b30a078e17ada71897b78f6ed3c0
StefanoRUsai <stefano.r.usai@gmail.com>
2018-02-07 14:54:01 +0100

    Aggiornamenti 7 febbraio push su master forzato233

--- app/src/main/java/com/example/st3/coinquiapp/activity/PersonalArea.java
+++ app/src/main/java/com/example/st3/coinquiapp/activity/PersonalArea.java
@@ -21,13 +21,12 @@ import com.example.st3.coinquiapp.R;
 
 public class PersonalArea extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.personal_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.personal_main);
+            Toolbar toolbar = (Toolbar) findViewById(R.id.toolbarP);
+            setSupportActionBar(toolbar);
 
        /* FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -38,71 +37,70 @@ public class PersonalArea extends AppCompatActivity
             }
         });*/
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                    this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            drawer.addDrawerListener(toggle);
+            toggle.syncState();
 
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_personale);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
+            NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+            navigationView.setNavigationItemSelectedListener(this);
         }
-    }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
+        @Override
+        public void onBackPressed() {
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            if (drawer.isDrawerOpen(GravityCompat.START)) {
+                drawer.closeDrawer(GravityCompat.START);
+            } else {
+                super.onBackPressed();
+            }
+        }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        @Override
+        public boolean onCreateOptionsMenu(Menu menu) {
+            // Inflate the menu; this adds items to the action bar if it is present.
+            getMenuInflater().inflate(R.menu.main, menu);
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+        @Override
+        public boolean onOptionsItemSelected(MenuItem item) {
+            // Handle action bar item clicks here. The action bar will
+            // automatically handle clicks on the Home/Up button, so long
+            // as you specify a parent activity in AndroidManifest.xml.
+            int id = item.getItemId();
 
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-        FragmentManager fragmentManager= getFragmentManager();
-        if (id == R.id.nav_casa) {
-            Intent searchIntent = new Intent(PersonalArea.this, MainActivity.class);
-            startActivity(searchIntent);
-            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
-        } else if (id == R.id.nav_personale) {
+            //noinspection SimplifiableIfStatement
+            if (id == R.id.action_settings) {
+                return true;
+            }
 
-            Intent searchIntent = new Intent(PersonalArea.this, PersonalArea.class);
-            startActivity(searchIntent);
-
-            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
-
+            return super.onOptionsItemSelected(item);
         }
 
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
+        @SuppressWarnings("StatementWithEmptyBody")
+        @Override
+        public boolean onNavigationItemSelected(MenuItem item) {
+            // Handle navigation view item clicks here.
+            int id = item.getItemId();
+            FragmentManager fragmentManager= getFragmentManager();
+            if (id == R.id.nav_casa) {
+                Intent searchIntent = new Intent(PersonalArea.this, MainActivity.class);
+                startActivity(searchIntent);
+
+                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+            } else if (id == R.id.nav_personale) {
+
+                Intent searchIntent2 = new Intent(PersonalArea.this, PersonalArea.class);
+                startActivity(searchIntent2);
+                overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+            }
+
+            DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+            drawer.closeDrawer(GravityCompat.START);
+            return true;
+        }
 
     public void onClick1(View view){
         Intent intent = new  Intent(getBaseContext(), Turni.class);
@@ -115,12 +113,12 @@ public class PersonalArea extends AppCompatActivity
     }
 
     public void onClick3(View view){
-        Intent intent = new  Intent(getBaseContext(), SpesePersonali.class);
+        Intent intent = new  Intent(getBaseContext(), Spese.class);
         startActivity(intent);
     }
 
     public void onClick4(View view){
-        Intent intent = new  Intent(getBaseContext(), Login_principale.class);
+        Intent intent = new  Intent(getBaseContext(), BachecaActivity.class);
         startActivity(intent);
     }
 
